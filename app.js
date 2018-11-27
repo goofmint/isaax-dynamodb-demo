63c67c7b1cf4cbf2d87f66af8b91b79d55b61411
Atsushi <atsushi@moongift.jp>
2018-11-27 11:49:48 +0900

    Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const deviceId = 'pi';
 
 AWS.config.loadFromPath('./config.json');
 
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB();
 
 const tableName = 'isaax';
 
@@ -14,15 +14,15 @@ setInterval(async () => {
     const params = {
       TableName: tableName,
       Item: {
-        device : deviceId,
-        value: 50,
-        timestamp: Date.now(),
-        id: Date.now().toString()
+        device : {S: deviceId},
+        value: {N: "50"},
+        timestamp: {N: Date.now().toString()},
+        id: {S: Date.now().toString()}
       }
     };
     const {stdout, stderr} = await promisify(exec)('vcgencmd measure_temp');
     const temp = parseFloat(stdout.replace(/temp=([0-9\.]*)'C/, '$1'));
-    const data = await promisify(dynamodb.put)(params);
+    const data = await promisify(dynamodb.putItem)(params);
     console.log(data);
   } catch (e) {
     console.log(e)
