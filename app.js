773cd26033c0e9aad5f096961e826195cf0d7185
Atsushi <atsushi@moongift.jp>
2018-11-27 11:52:20 +0900

    Update

--- app.js
+++ app.js
@@ -10,21 +10,19 @@ const dynamodb = new AWS.DynamoDB();
 const tableName = 'isaax';
 
 setInterval(async () => {
-  try {
-    const params = {
-      TableName: tableName,
-      Item: {
-        device : {S: deviceId},
-        value: {N: "50"},
-        timestamp: {N: Date.now().toString()},
-        id: {S: Date.now().toString()}
-      }
-    };
-    const {stdout, stderr} = await promisify(exec)('vcgencmd measure_temp');
-    const temp = parseFloat(stdout.replace(/temp=([0-9\.]*)'C/, '$1'));
-    const data = await promisify(dynamodb.putItem)(params);
-    console.log(data);
-  } catch (e) {
-    console.log(e)
-  }
+  const params = {
+    TableName: tableName,
+    Item: {
+      device : {S: deviceId},
+      value: {N: "50"},
+      timestamp: {N: Date.now().toString()},
+      id: {S: Date.now().toString()}
+    }
+  };
+  const {stdout, stderr} = await promisify(exec)('vcgencmd measure_temp');
+  const temp = parseFloat(stdout.replace(/temp=([0-9\.]*)'C/, '$1'));
+  dynamodb.putItem(params).promise()
+    .then((err, data) => {
+      console.log(data);
+    });
 }, 5000);
